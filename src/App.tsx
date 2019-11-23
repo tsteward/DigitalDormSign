82821c88b8f4af5906ec81a8db11ab0b9afdb639
Joseph Naberhaus <naberhausj@gmail.com>
2019-11-23 17:16:30 -0600

    Initialize app

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Testing123
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
