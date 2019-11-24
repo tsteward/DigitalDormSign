b86a601f64338e786a731c602325e37f6ebe1d0b
Joseph Naberhaus <naberhausj@gmail.com>
2019-11-23 18:11:17 -0600

    Add backend and test code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './App.css';
+import {Title} from "./title/Title";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Testing123
-        </p>
-      </header>
-    </div>
-  );
-}
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Title/>>
+				<p>
+					Testing123
+				</p>
+			</header>
+		</div>
+	);
+};
 
 export default App;
---
+++ frontend/src/title/Title.tsx
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+export class Title extends Component {
+	render() {
+		return (
+			<h1>Welcome to 2280 Stanton</h1>
+		);
+	}
+}
